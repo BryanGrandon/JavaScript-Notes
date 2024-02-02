7d7aa052c0966521b0b0ce27a2acbe8f754e6ee1
Bryan Grandon <bryan.grandon.c@gmail.com>
2024-02-02 17:30:13 -0300

    feat(web):change control-flow folder with its files

---
+++ code/control-flow/if.js
@@ -0,0 +1,18 @@
+let value = 5;
+
+// if
+if (value == 1) {
+  console.log("IF");
+}
+// else if
+else if (value == 2) {
+  console.log("ELSE IF (1)");
+} else if (value == 3) {
+  console.log("ELSE IF (2)");
+} else if (value == 4) {
+  console.log("ELSE IF (3)");
+}
+// else
+else {
+  console.log("Last option");
+}
---
+++ code/control-flow/switch.js
@@ -0,0 +1,18 @@
+let option = Math.round(Math.random() * 3);
+switch (option) {
+  case 1:
+    console.log(`If the option is 1`);
+    break;
+
+  case 2:
+    console.log(`If the option is 2`);
+    break;
+
+  case 3:
+    console.log(`If the option is 3`);
+    break;
+
+  default:
+    console.log(`option default`);
+    break;
+}
