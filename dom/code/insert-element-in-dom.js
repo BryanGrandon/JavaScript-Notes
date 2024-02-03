c83d445ca61b7dbdcbf47a5b5437203c53b37a7f
Bryan Grandon <bryan.grandon.c@gmail.com>
2024-02-03 08:18:29 -0300

    refactor(web):change insert-element-in-dom.js in the DOM

--- dom/code/insert-element-in-dom.js
+++ dom/code/insert-element-in-dom.js
@@ -16,28 +16,20 @@ const d = document;
 
 // -------- Set content -------- //
 
-// ---- innerText ---- //
-// Respecting the blank spaces
-
+// innerText // Respecting the blank spaces
 const $element1 = d.querySelector(".inner-text");
 $element1.innerText = text;
 
-// ---- textContent ---- //
-// Does not respect blank spaces
-
+// textContent // Does not respect blank spaces
 const $element2 = d.querySelector(".text-content");
 $element2.textContent = text;
 
 // -------- Set HTML -------- //
 
-// ---- innerHTML ---- //
-// Inside element
-
+// innerHTML // Inside element
 const $element3 = d.querySelector(".outer-html");
 $element3.innerHTML = text;
 
-// ---- outerHTML ---- //
-// Element replacement
-
+// outerHTML // Element replacement
 const $element4 = d.querySelector(".inner-html");
 $element4.outerHTML = text;
