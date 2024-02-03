bfbddca3ddd53866e871ebb5b4641e04d8f1ebb9
Bryan Grandon <bryan.grandon.c@gmail.com>
2024-02-03 07:15:22 -0300

    refactor(web):rename manager-dom-attribute.js to attribute-manager.js

---
+++ dom/code/attribute-manage.js
@@ -0,0 +1,27 @@
+"use strict";
+const $link = document.querySelector(".link");
+
+/*/ hasAttribute(attr) //
+      Indicates whether the element has the HTML attr attribute.
+*/
+const $hasAttribute = $link.hasAttribute("href");
+
+/*/ getAttribute(attr) //
+      Returns the value of the attr attribute of the element or null if it does not exist.
+*/
+const $getAttribute = $link.getAttribute("href");
+
+/*/ setAttribute(attr, value) //
+      Adds or changes the attr attribute to the value value of the HTML element.
+*/
+$link.setAttribute("href", "https://pixabay.com/");
+
+/*/ toggleAttribute(attr, forge) //
+      Adds attr attribute if it does not exist, if it does exist, removes it.
+*/
+$link.toggleAttribute("target", "_blank");
+
+/*/ removeAttribute(attr) //
+      Removes the attr attribute from the HTML element.
+*/
+$link.removeAttribute("target");
