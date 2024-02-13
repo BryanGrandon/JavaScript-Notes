ae0aedd1061c60d16ee7f55bc3314f9545543f05
Bryan Grandon <bryan.grandon.c@gmail.com>
2024-02-13 02:48:13 -0300

    refactor(web):modify the files in the code folder

--- projects/code/project.js
+++ projects/code/project.js
@@ -2,10 +2,5 @@ import { navbar } from "./js/navbar.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   // Navbar (button, button Icon, dropdown, class Active)
-  navbar(
-    ".button--navbar",
-    ".button--navbar i",
-    ".dropdown",
-    "dropdown--active"
-  );
+  // navbar();
 });
