83a52ff4e1d0afd644a15c99136d2b21e40e3244
Bryan Grandon <bryan.grandon.c@gmail.com>
2024-02-13 15:15:39 -0300

    feat(web):add navbar in the code folder

--- projects/code/project.js
+++ projects/code/project.js
@@ -1,6 +1,1 @@
-import { navbar } from "./js/navbar.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  // Navbar (button, button Icon, dropdown, class Active)
-  // navbar();
-});
+document.addEventListener("DOMContentLoaded", () => {});
