eed39d14f8d63348eaa050784c842aa544d5bda5
Bryan Grandon <bryan.grandon.c@gmail.com>
2024-05-07 13:44:16 -0400

    (web):remove the folder others

--- projects/others/navbar/navbar-2/navbar-2.js
+++
@@ -1,9 +0,0 @@
-const $dropdown = document.querySelector(".dropdown");
-const $button = document.querySelector(".button--navbar");
-const $buttonIcon = document.querySelector(".button--navbar i");
-
-$button.addEventListener("click", () => {
-  $dropdown.classList.toggle("dropdown--active");
-  const isOpen = $dropdown.classList.contains("dropdown--active");
-  $buttonIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
-});
