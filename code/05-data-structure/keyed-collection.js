5217cf8e0e603f94e2f68a4a9949cbe198fb9b58
Bryan Grandon <bryan.grandon.c@gmail.com>
2024-05-30 08:36:33 -0400

    refactor(web):change files of the code folder

--- code/05-data-structure/keyed-collection.js
+++ code/05-data-structure/keyed-collection.js
@@ -9,6 +9,7 @@ const methodsMapOrSet = {
   delete: map.delete("key"),
   clear: map.clear(),
 };
+
 let getOfValueMap = map.get("key");
 const weakMap = new WeakMap(); // weakMap
 let object = { name: "Bryan" }; // Define the Value and the key has to be an object
@@ -22,9 +23,11 @@ const arr = [1, 2, 3];
 set.add(2);
 set.add(2); // Does not support repeated values
 set.add(arr);
+
 const weakSet = new WeakSet(); // weakSet
 const obj_1 = { name: "obj 1" };
 const obj_2 = { name: "obj 2" };
+
 weakSet.add(obj_1);
 weakSet.add(obj_1);
 weakSet.add(obj_2);
